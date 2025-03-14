ce683fe4f87c3a66f8b57ea0d60cccc0d076b385
Damian Kucharski <c.DamianKucharski@gmail.com>
2025-03-14 23:06:01 +0100

    entry for callbacks and giraffe class

---
+++ giraffe/callback.py
@@ -0,0 +1,17 @@
+from giraffe.giraffe import Giraffe
+
+class Callback:
+    def __init__(self) -> None:
+        pass
+
+    def on_generation_end(self, giraffe: Giraffe) -> None:
+        pass
+
+    def on_evolution_end(self, giraffe: Giraffe) -> None:
+        pass
+
+    def on_evolution_start(self, giraffe: Giraffe) -> None:
+        pass
+
+    def on_generation_start(self, giraffe: Giraffe) -> None:
+        pass
