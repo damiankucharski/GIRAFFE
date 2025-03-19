304c75b2d15840aabd032f65136892e2d71abf86
damiankucharski <c.damiankucharski@gmail.com>
2025-03-19 14:41:29 +0100

    Move imports in fitness functions so that they are not importable and add additional giraffe args plus input shape checking

--- giraffe/fitness.py
+++ giraffe/fitness.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn.metrics import average_precision_score, roc_auc_score
 
 from giraffe.globals import BACKEND as B
 from giraffe.tree import Tree
@@ -7,12 +6,16 @@ from giraffe.types import Tensor
 
 
 def average_precision_fitness(tree: Tree, gt: Tensor):
+    from sklearn.metrics import average_precision_score
+
     pred_np: np.ndarray = B.to_numpy(tree.evaluation)
     gt_np: np.ndarray = B.to_numpy(B.to_float(gt))
     return average_precision_score(gt_np, pred_np)
 
 
 def roc_auc_score_fitness(tree: Tree, gt: Tensor):
+    from sklearn.metrics import roc_auc_score
+
     pred_np: np.ndarray = B.to_numpy(tree.evaluation)
     gt_np: np.ndarray = B.to_numpy(B.to_float(gt))
     return roc_auc_score(gt_np, pred_np)
--- giraffe/giraffe.py
+++ giraffe/giraffe.py
@@ -1,9 +1,13 @@
 from pathlib import Path
-from typing import Iterable, Union
+from typing import Callable, Iterable, Union
+
+import numpy as np
 
 from giraffe.backend.backend import Backend
 from giraffe.globals import BACKEND as B
 from giraffe.globals import DEVICE
+from giraffe.tree import Tree
+from giraffe.types import Tensor
 
 
 class Giraffe:
@@ -11,12 +15,25 @@ class Giraffe:
         self,
         preds_source: Union[Path, str, Iterable[Path], Iterable[str]],
         gt_path: Union[Path, str],
+        population_size: int,
+        population_multiplier: int,
+        tournament_size: int,
+        fitness_function: Callable[[Tree, Tensor], float],
         backend: Union[Backend, None] = None,
+        seed: int = 0,
     ):
         if backend is not None:
             Backend.set_backend(backend)
+        if seed is not None:
+            np.random.seed(seed)
+
+        self.population_size = population_size
+        self.population_multiplier = population_multiplier
+        self.tournament_size = tournament_size
+        self.fitness_function = fitness_function
 
         self.train_tensors, self.gt_tensor = self._build_train_tensors(preds_source, gt_path)
+        self._validate_input()
 
     def _build_train_tensors(self, preds_source, gt_path):
         if isinstance(preds_source, str):
@@ -32,3 +49,17 @@ class Giraffe:
 
         gt_tensor = B.load(gt_path, DEVICE)
         return train_tensors, gt_tensor
+
+    def _validate_input(self, fix_swapped=True):  # no way to change this argument for now TODO
+        # check if all tensors have the same shape
+        shapes = [B.shape(tensor) for tensor in self.train_tensors.values()]
+        if len(set(shapes)) > 1:
+            raise ValueError(f"Tensors have different shapes: {shapes}")
+
+        if B.shape(self.gt_tensor) != shapes[0]:
+            if fix_swapped:
+                if (shapes[0] == B.shape(self.gt_tensor)[::-1]) and (len(shapes[0]) == 2):
+                    self.gt_tensor = B.reshape(self.gt_tensor, shapes[0])
+
+            else:
+                raise ValueError(f"Ground truth tensor has different shape than input tensors: {shapes[0]} != {B.shape(self.gt_tensor)}")
